7038232b2f4f497f63127207882313376861dc0e
phantuyenhy@gmail.com <phantuyenhy@gmail.com@3051304b-cd5f-c7cb-f2c9-3ad7fcdf282c>
2010-08-12 10:27:02 +0000

    Design chuan

---
+++ trunk/doctorweb4rum/Sources/DoctorsWebForum/App_Code/DAL/DataHelper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Configuration;
+namespace DAL
+{
+    public class DataHelper
+    {
+        public static String dataAccessStringType = ConfigurationManager.AppSettings["DataAccessLayerType"];
+        public DataHelper()
+        {
+
+        }
+
+        //public static ICommentDA GetCommentDA()
+        //{
+        //    ICommentDA dc = null;
+        //    if (String.IsNullOrEmpty(dataAccessStringType))
+        //    {
+        //        throw (new NullReferenceException("DataAccessType in Web.config is null or empty"));
+        //    }
+        //    else
+        //    {
+        //        if (dataAccessStringType.Equals("SQLSERVER"))
+        //        {
+        //            Type t = Type.GetType("DAL.CommentDA");
+        //            dc = (CommentDA)Activator.CreateInstance(t);
+        //        }
+        //    }
+        //    return dc;
+        //}
+
+        //public static IRateDA GetRateDA()
+        //{
+        //    IRateDA dc = null;
+        //    if (String.IsNullOrEmpty(dataAccessStringType))
+        //    {
+        //        throw (new NullReferenceException("DataAccessType in Web.config is null or empty"));
+        //    }
+        //    else
+        //    {
+        //        if (dataAccessStringType.Equals("SQLSERVER"))
+        //        {
+        //            Type t = Type.GetType("DAL.RateDA");
+        //            dc = (RateDA)Activator.CreateInstance(t);
+        //        }
+        //    }
+        //    return dc;
+        //}
+
+
+        //public static ICategoryDA GetCategoryDA()
+        //{
+        //    ICategoryDA dc = null;
+        //    if (String.IsNullOrEmpty(dataAccessStringType))
+        //    {
+        //        throw (new NullReferenceException("DataAccessType in Web.config is null or empty"));
+        //    }
+        //    else
+        //    {
+        //        if (dataAccessStringType.Equals("SQLSERVER"))
+        //        {
+        //            Type t = Type.GetType("DAL.CategoryDA");
+        //            dc = (CategoryDA)Activator.CreateInstance(t);
+        //        }
+        //    }
+        //    return dc;
+        //}
+
+        //public static IArticlesDA GetArticlesDA()
+        //{
+        //    IArticlesDA dc = null;
+        //    if (String.IsNullOrEmpty(dataAccessStringType))
+        //    {
+        //        throw (new NullReferenceException("DataAccessType in Web.config is null or empty"));
+        //    }
+        //    else
+        //    {
+        //        if (dataAccessStringType.Equals("SQLSERVER"))
+        //        {
+        //            Type t = Type.GetType("DAL.ArticlesDA");
+        //            dc = (ArticlesDA)Activator.CreateInstance(t);
+        //        }
+        //    }
+        //    return dc;
+        //}
+
+        //public static ICustomersDA GetCustomersDA()
+        //{
+        //    ICustomersDA dc = null;
+        //    if (String.IsNullOrEmpty(dataAccessStringType))
+        //    {
+        //        throw (new NullReferenceException("DataAccessType in Web.config is null or empty"));
+        //    }
+        //    else
+        //    {
+        //        if (dataAccessStringType.Equals("SQLSERVER"))
+        //        {
+        //            Type t = Type.GetType("DAL.CustomerDA");
+        //            dc = (CustomerDA)Activator.CreateInstance(t);
+        //        }
+        //    }
+        //    return dc;
+        //}
+    }
+}
