aeb5c0121013a50c0bfafa4a319d2af4fa5a583e
nampx153@gmail.com <nampx153@gmail.com@3051304b-cd5f-c7cb-f2c9-3ad7fcdf282c>
2010-08-30 09:51:02 +0000

    Da co MasterPageAdmin.master

---
+++ doctorweb4rum/Sources/DoctorsWebForum/GUI/Admin/Admin.aspx
@@ -0,0 +1,6 @@
+<%@ Page Language="C#" MasterPageFile="~/GUI/Admin/MasterPageAdmin.master" AutoEventWireup="true" CodeFile="Admin.aspx.cs" Inherits="GUI_Admin_Admin" Title="Untitled Page" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+  ABC
+</asp:Content>
+
---
+++ doctorweb4rum/Sources/DoctorsWebForum/GUI/Admin/MasterPageAdmin.master.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Data;
+using System.Configuration;
+using System.Collections;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using BLL;
+
+public partial class GUI_Admin_MasterPageAdmin : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void CustomValidator1_ServerValidate(object source, ServerValidateEventArgs args)
+    {
+        string userName = txtUserName.Text;
+        string password = txtPassword.Text;
+        Member member = MemberBLL.GetMemberByUserNamePassword(userName, password);
+        if (member == null)
+        {
+            args.IsValid = false;
+        }
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        if (!Page.IsValid)
+        {
+            ModalPopupExtenderLogin.Enabled = true;
+            ModalPopupExtenderLogin.Show();
+        }
+        if (Page.IsValid)
+        {
+            string userName = txtUserName.Text;
+            string password = txtPassword.Text;
+            if (userName.Length > 0 || userName != null || password.Length > 0 || password != null)
+            {
+                Member member = MemberBLL.GetMemberByUserNamePassword(userName, password);
+                if (member != null)
+                {
+                    MemberProfile memberProfile = MemberBLL.GetMemberProfileByMemberID(member.MemberID);
+                    if (memberProfile != null)
+                    {
+                        Role role = RoleBLL.GetRoleByRoleID(memberProfile.RoleID);
+                        if (role != null)
+                        {
+                            if (role.RoleName.Equals("Member"))
+                            {
+                                Response.Redirect("Index.aspx");
+                            }
+                            if (role.RoleName.Equals("Moderator"))
+                            {
+
+                            }
+                            if (role.RoleName.Equals("Super Moderator"))
+                            {
+
+                            }
+                            if (role.RoleName.Equals("Admin"))
+                            {
+
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    //Response.Redirect("Login.aspx");
+                }
+            }
+            else
+            {
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+    }
+    protected void btnCancel_Click(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSearch_Click(object sender, ImageClickEventArgs e)
+    {
+
+    }
+}
